9dc8bbf263fd56f68a01683df15edf2358a7ffdd
joy <apple@appurunoMac-mini.local>
2023-01-31 11:52:26 +0600

    added kitchen_timing

---
+++ codechef/cost_of_groceries.py
@@ -0,0 +1,13 @@
+i = input
+
+# cook your dish here
+for _ in range(int(i())):
+    n, x = map(int, i().split())
+    freshness_value_of_items = list(map(int, i().split()))
+    cost_of_each_item = list(map(int, i().split()))
+    res = 0
+    for idx in range(len(freshness_value_of_items)):
+        if freshness_value_of_items[idx] >= x:
+            res += cost_of_each_item[idx]
+
+    print(res)
