2a23081332223a09eaa7240a326240f921c9ad28
jahir-raihan <raihan.joy10@gmail.com>
2023-06-16 12:49:13 +0600

    Added insert delete getrandom problem

--- leetcode/insert delete getrandom.py
+++ leetcode/insert delete getrandom.py
@@ -1,6 +1,32 @@
-nums = {1:0, 2:1, 3:1}
 import random
 
-lst = [k for k in nums.keys() if nums[k] > 0]
-random_val = random.randint(0, len(lst) - 1)
-print(lst[random_val])
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.set = {}
+
+    def insert(self, val: int) -> bool:
+        try:
+            tmp = self.set[val]
+            self.set[val] = 1
+            if tmp == 0:
+                return True
+            return False
+        except:
+            self.set[val] = 1
+            return True
+
+    def remove(self, val: int) -> bool:
+        try:
+            tmp = self.set[val]
+            if tmp > 0:
+                self.set[val] = 0
+                return True
+            return False
+        except:
+            return False
+
+    def getRandom(self) -> int:
+        lst = [k for k in self.set.keys() if self.set[k] > 0]
+        return lst[random.randint(0, len(lst) - 1)]
