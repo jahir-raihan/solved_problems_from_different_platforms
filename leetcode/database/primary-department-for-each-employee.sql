6634066096eff18ba0d8530221b4f71dd8e4055b
jahir-raihan <raihan.joy10@gmail.com>
2023-06-26 12:20:14 +0600

    primary department for each employee

---
+++ leetcode/database/primary-department-for-each-employee.sql
@@ -0,0 +1,13 @@
+select employee_id, department_id
+from employee
+where primary_flag = 'Y'
+union
+select employee_id, department_id
+from employee
+where employee_id not in(
+    select employee_id
+    from employee
+    where primary_flag = 'Y'
+
+)
+order by employee_id
