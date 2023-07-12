1838b6d7a459b2fae144905980bf7f5f86cba837
jahir-raihan <raihan.joy10@gmail.com>
2023-07-12 12:40:40 +0600

    Trying to solve zig zag conversion

--- leetcode/zig-zag-conversion.py
+++ leetcode/zig-zag-conversion.py
@@ -1,8 +1,30 @@
 s = 'PAYPALISHIRING'
 length = len(s)
-numRows = 3
+rows = 3
+gap = rows - 2
+lst = [[0 for _ in range(length//2)] for _ in range(rows)]
+counter = 0
+for i in range(0, length, rows+gap):
+    tmp_str = s[i:i+rows]
+    for j in range(len(tmp_str)):
+        lst[j][counter] = tmp_str[j]
+    counter += gap + 1
 
-gap_elem = numRows - 2
+counter = 1
+for i in range(rows, length, rows+gap):
+    tmp_str = s[i:i+gap]
+    cur_idx = rows-2
+    for j in range(len(tmp_str)):
+        lst[cur_idx][counter] = tmp_str[j]
+        cur_idx -= 1
+        counter += 1
+    counter += 1
 
-lst = [[0]*(length//2)]*numRows
-print(lst)
+
+
+res = ''
+for i in lst:
+    for j in i:
+        if j != 0:
+            res += j
+print(res)
