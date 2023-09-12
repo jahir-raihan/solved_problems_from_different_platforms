fd83f8f0eaff7b4e732742d4acfa9d02cbb51edd
jahir-raihan <raihan.joy10@gmail.com>
2023-09-12 10:54:30 +0600

    make-it-zero

---
+++ codeforces/make-it-zero-codeforces.py
@@ -0,0 +1,15 @@
+t = int(input())
+for _ in range(t):
+
+    n = int(input())
+    input()
+    if n & 1:
+        print(4)
+        print("1", n - 1)
+        print("1", n - 1)
+        print(n - 1, n)
+        print(n - 1, n)
+    else:
+        print("2")
+        print("1 ", n)
+        print("1", n)
