740f57cf31e9516147e3a80d7c92ef61bedb0120
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-01-28 21:18:15 +0600

    product-analysis-iii

---
+++ leetcode/database/product-analysis-iii.sql
@@ -0,0 +1,7 @@
+select product_id, product_name
+from Product
+where product_id in (
+    select product_id from Sales where sale_date between '2019-01-01' and '2019-03-31'
+) and product_id not in (
+    select product_id from sales where sale_date > '2019-03-31' or sale_date < '2019-01-01'
+)
