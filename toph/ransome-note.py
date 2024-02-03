fc428d37de6b09215e0bb9752c8473049c750999
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-02-03 22:53:28 +0600

    Ransom Note

--- toph/ransome-note.py
+++ toph/ransome-note.py
@@ -1,28 +1,15 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ran = {}
+t = int(input())
 
-        for c in ransomNote:
-            try:
-                ran[c] += 1
-            except:
-                ran[c] = 1
-
-        mag = {}
-        for c in magazine:
-            try:
-                mag[c] += 1
-            except:
-                mag[c] = 1
-
-        res = True
-
-        for k, v in ran.items():
-            try:
-                if mag[k] < v:
-                    res = False
-                    break
-            except:
-                res = False
-
-        return res
+for _ in range(t):
+    n = int(input())
+    a = input()
+    b = input()
+    zero = 0
+    one = 0
+    for i in range(n):
+        if a[i] != b[i]:
+            if a[i] == '0':
+                zero += 1
+            else:
+                one += 1
+    print(max(zero, one))
