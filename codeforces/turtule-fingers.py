adf5ac1b8c70792928954a396b182344a17a9078
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-03-03 23:08:29 +0600

    turtule-fingers

---
+++ codeforces/turtule-fingers.py
@@ -0,0 +1,8 @@
+for _ in range (int (input())):
+    a,b,l = map(int,input().split())
+    st = set()
+    for i in range (21):
+        for j in range (21):
+            if (l%(a**i * b**j) == 0):
+                st.add(l//(a**i * b**j))
+    print (len(st))
