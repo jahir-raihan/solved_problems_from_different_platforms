cfd1bde3476fe73971d101ac21476bbf60badd53
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-04-09 22:12:15 +0600

    interpreter

---
+++ solved_in_mobile/interpreter copy.py
@@ -0,0 +1,33 @@
+def interpret(value,command,args):
+	prec ={'+':1,'-':1,'*':2}
+	evalu={'+': lambda x,y:x+y,
+	'-':lambda x,y:x-y,
+	'*':lambda x,y:x*y}
+		
+	if len(command)>1:
+		def interpret1(value,command,args):
+			if prec[command[1]]>prec[command[0]]:
+				n = evalu[command[1]](value,args[0])
+				args.pop(0)
+				command.pop(1)		
+			elif prec[command[0]]>prec[command[1]]:
+					n = evalu[command[0]](value,args[0])
+					args.pop(0)
+					command.pop(0)
+			elif prec[command[0]]==prec[command[1]]:
+				n = evalu[command[0]](value,args[0])
+				args.pop(0)
+				command.pop(0)
+								
+			interpret(n,command,args)
+								
+		interpret1(value,command,args)
+							
+	else:
+			n = evalu[command[0]](value,args[0])
+			print(n)	
+
+				
+interpret(4,['-','+'],[2,16])#4 - 2 +16 = 18
+interpret(2,['+','*'],[4,2])#2*4+2 = 10
+interpret(1,['+','*','+','-'],[1,3,2,2])
