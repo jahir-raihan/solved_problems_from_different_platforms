16beffd4e320ae62bbb488d81aeb80ed87e1a5d1
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-05-14 23:35:42 +0600

    copy solution of path with maximum gold

--- leetcode/path-with-maximum-gold.py
+++ leetcode/path-with-maximum-gold.py
@@ -1,46 +1,22 @@
-def getMaximumGold(grid) -> int:
-    max_sum = 0
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        DIR = [0, 1, 0, -1, 0]
 
-    m, n = len(grid), len(grid[0])
+        def dfs(r, c):
+            if r < 0 or r == m or c < 0 or c == n or grid[r][c] == 0:
+                return 0
+            ans = 0
+            orgGold = grid[r][c]
+            grid[r][c] = 0
+            for i in range(4):
+                ans = max(ans, dfs(r + DIR[i], c + DIR[i + 1]))
+            grid[r][c] = orgGold
+            return ans + grid[r][c]
 
-    def get_max_sum(i, j, vis):
-
-        cur_val = grid[i][j]
-
-        q = []
-        if i < m-1 and grid[i + 1][j] != 0 and (i + 1, j) not in vis:
-            q.append((i + 1, j))
-        if i > 0 and grid[i - 1][j] != 0 and (i - 1, j) not in vis:
-            q.append((i - 1, j))
-        if j < n-1 and grid[i][j + 1] != 0 and (i, j + 1) not in vis:
-            q.append((i, j + 1))
-        if j > 0 and grid[i][j - 1] != 0 and (i, j - 1) not in vis:
-            q.append((i, j - 1))
-
-        vis.add((i, j))
-
-        local_max = 0
-        for val in q:
-            local_max = max(local_max, get_max_sum(val[0], val[1], vis))
-
-        return cur_val + local_max
-
-    for i in range(m):
-        for j in range(n):
-            if grid[i][j] != 0:
-
-                max_sum = max(max_sum, get_max_sum(i, j, set()))
-
-    return max_sum
-
-
-grid = [[1, 0, 7, 0, 0, 0],
-       [2, 0, 6, 0, 1, 0],
-       [3, 5, 6, 7, 4, 2],
-       [4, 3, 1, 0, 2, 0],
-       [3, 0, 5, 0, 20, 0]]
-
-res = getMaximumGold(grid)
-
-print(res)
-
+        ans = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] != 0:
+                    ans = max(ans, dfs(r, c))
+        return ans
