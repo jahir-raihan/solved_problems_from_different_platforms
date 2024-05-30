a720a250e86c8d23495e8d3ada9a3d9e93ba78b7
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-05-30 21:08:13 +0600

    3sum-recaped

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -1,23 +1,21 @@
-res = []
-v = 9999999999
-lst = [-23904,78409,-7418,77916]
+class Solution:
+    def threeSum(self, lst: List[int]) -> List[List[int]]:
+        res = []
+        lst.sort()
+        for i, a in enumerate(lst):
+            if i > 0 and a == lst[i - 1]:
+                continue
+            j, k = i + 1, len(lst) - 1
 
-
-for i,a in enumerate(lst):
-    if i > 0 and a == lst[i-1]:
-        continue
-    j, k = i+1, len(lst)-1
-
-    while j < k:
-        tmp = lst[i] + lst[j] + lst[k]
-        if tmp > 0:
-            k -= 1
-        elif tmp < 0:
-            j += 1
-        else:
-            res.append([lst[i], lst[j], lst[k]])
-            j += 1
-            while lst[j] == lst[j-1] and j < k:
-                j += 1
-
-print(res)
+            while j < k:
+                tmp = lst[i] + lst[j] + lst[k]
+                if tmp > 0:
+                    k -= 1
+                elif tmp < 0:
+                    j += 1
+                else:
+                    res.append([lst[i], lst[j], lst[k]])
+                    j += 1
+                    while lst[j] == lst[j - 1] and j < k:
+                        j += 1
+        return res
