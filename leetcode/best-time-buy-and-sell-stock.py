3cae85ec12a96ce677601cd2c07acad1fb0a13fb
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-09-11 22:30:02 +0600

    best time to buy and sell stock

--- leetcode/best-time-buy-and-sell-stock.py
+++ leetcode/best-time-buy-and-sell-stock.py
@@ -12,3 +12,18 @@ class Solution:
             else:
                 cur_val = prices[i]
         return res
+
+# new
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        i, j = 0, 1
+        while j < len(prices):
+            if prices[i] > prices[j]:
+                i = j
+                j = i + 1
+            else:
+                current_price = prices[j] - prices[i]
+                max_profit = max_profit if max_profit > current_price else current_price
+                j += 1
+        return max_profit
