633fd6493e76e1c63ba4ccc1a5a253d85cebf303
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-09-11 21:36:57 +0600

    valid_parentheses updated

--- leetcode/valid_paranthesis.py
+++ leetcode/valid_paranthesis.py
@@ -8,3 +8,16 @@ for i in s:
     else:
         break
 
+
+# Updated
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        mapper = {')': '(', ']': '[', '}': '{'}
+        for i in s:
+            if i in (')', ']', '}') and stk and mapper[i] == stk[-1]:
+                stk.pop(-1)
+            else:
+                stk.append(i)
+
+        return not stk
