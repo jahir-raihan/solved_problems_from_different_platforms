63b61cdb21c8165746de4a9002e0032e7439df5c
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-09-11 22:42:05 +0600

    valid_palindrome new

--- leetcode/valid_palindrome.py
+++ leetcode/valid_palindrome.py
@@ -11,3 +11,19 @@ while i < j:
         res = False
         break
 
+# New
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        new_str = ''
+        for i in s:
+            if i.isalnum():
+                new_str += i.lower() if i.isalpha() else i
+
+        i, j = 0, len(new_str) - 1
+        while i < j:
+            if new_str[i] != new_str[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
